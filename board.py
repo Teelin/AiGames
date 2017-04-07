02bce86be2ba6a884fa2f30a041136de47a0b4f8
MrTeeling-PC\MrTeeling <teg@springfield-sec.portsmouth.sch.uk>
2017-04-07 15:14:36 +0100

    Working on table with output (works)

--- board.py
+++ board.py
@@ -3,29 +3,66 @@ import random
 
 class board:
 
+    def __init__(self):
+        self.VMB = ""
+        self.table =[]
+
     def setMboard(self, mbstr):
         
         mbList = mbstr.split(",")
         validM = []
-        for i in range(0,(len(mbList)-1)):
-            if mbList[i] == "-1":
+        for i in range(0, len(mbList)):
+            if mbList[i] == "-1" or mbList[i] == "-1\n":
                 validM.append(i)
+        
         if len(validM) == 1:
-            VMB = str(validM[0])
+            self.VMB = str(validM[0])
             check = "single"
+            
+        elif len(validM) > 1 and len(validM)<9:
+            check = "Multiple"
+            self.choice = random.choice(validM)
+                  
         else:
-            VMB = str(random.randint(0,8))
+            self.VMB = str(random.randint(0,8))
             check = "random"
             
         with open('mbfile.txt', 'a') as the_file:
-            the_file.write(check + VMB+'\n')
+            the_file.write(check + str(self.VMB)+'\n')
         
 
-    def setBoard():
-        return
+    def setBoard(self,bstr):
+        bList = bstr.split(",")
+        
+        a = ["0","1","2","9","10","11","18","19","20"]
+        b = ["3","4","5","12","13","14","21","22","23"]
+        c = ["6","7","8","15","16","17","24","25","26"]
+        d = ["27","28","29","36","37","38","45","46","47"]
+        e = ["30","31","32","39","40","41","48","49","50"]
+        f = ["33","34","35","42","43","44","51","52","53"]
+        g = ["54","55","56","63","64","65","72","73","74"]
+        h = ["57","58","59","66","67","68","75","76","77"]
+        i = ["60","61","62","69","70","71","78","79","80"]
+        self.table.append(a)
+        self.table.append(b)
+        self.table.append(c)
+        self.table.append(d)
+        self.table.append(e)
+        self.table.append(f)
+        self.table.append(g)
+        self.table.append(h)
+        self.table.append(i)
+        for i in range(0,len(self.table)):
+            test = self.table[i]
+            for j in test:
+                ind = test.index(j)
+                
+                self.table[i][ind] = bList[int(j)]
 
-    def getMboard():
-        return
-
-    def getBoard():
-        return
+        with open('bfile.txt', 'a') as the_file:
+            for i in self.table:
+                for j in range (0,len(i)):
+                    the_file.write(str(i[j]))
+                    if j == 2 or j == 5 or j == 8:
+                        the_file.write("\n")
+                the_file.write("\n")
--- mybot.py
+++ mybot.py
@@ -1,9 +1,6 @@
 import sys, fileinput
-<<<<<<< HEAD
-import io
-=======
 from board import board
->>>>>>> 895fd383680899f05ce288be8eec8e83f9ae3133
+
 
 with open('somefile.txt', 'a') as the_file:
     the_file.seek(0)
@@ -12,48 +9,7 @@ with open("settings.txt","a+") as file:
     file.seek(0)
     file.truncate()
 
-<<<<<<< HEAD
-#while True:
 
-for line in fileinput.input():
-    with open('somefile.txt', 'a') as the_file:
-        the_file.write( line+'\n')
-
-    parts = line.split(" ")
-
-    if parts[0] == "settings":
-        #store settings
-        if parts[1] == "timebank":
-            timebank = int(parts[2])
-            
-        elif parts[1] == "time_per_move":
-            time_per_move = int(parts[2])
-            
-        elif parts[1] == "player_names":
-            player_names = parts[2]
-            
-        elif parts[1] == "your_bot":
-            mybot = parts[2]
-            
-        elif parts[1] == "your_botid":
-            mybotid = int(parts[2])
-
-
-    elif parts[0] == "update":
-        #store settings
-        print("test")
-
-    elif parts[0] == "action":
-        #print("place_move 0 0")        
-
-        
-        io.IOBase.write("place_move 0 0")
-        io.flush()
-        
-        with open("settings.txt","a+") as file:
-            file.write(str(timebank) + str(time_per_move) + player_names + mybot + str(mybotid))
-
-=======
 while True:
 
     currentboard = board()
@@ -87,13 +43,14 @@ while True:
             if parts[2] == "macroboard":
                 mbstr = parts[3]
                 currentboard.setMboard(mbstr)
-            print("test")
+            elif parts[2] == "field":
+                bstr = parts[3]
+                currentboard.setBoard(bstr)
+            
 
         elif parts[0] == "action":
-            sys.stdout.write('place_move 0 0\n')
+            sys.stdout.write('place_move 0 1\n')
             sys.stdout.flush()
             with open("settings.txt","a+") as file:
                 file.write(str(timebank) +"\n"+ str(time_per_move) +"\n"+ player_names + mybot + str(myid)+"\n\n\n")
 
-    
->>>>>>> 895fd383680899f05ce288be8eec8e83f9ae3133
